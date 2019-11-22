3c85793fe66f7c1c421b94e8f96ee19efaebf2ef
Teresa Krohn <krohn.teresa@gmail.com>
2019-11-22 22:27:35 +0100

    solve: 2015, day 1, part 2

--- 2015/day01-not-quite-lisp.py
+++ 2015/day01-not-quite-lisp.py
@@ -14,22 +14,41 @@
 #* ()) and ))( both result in floor -1 (the first basement level).
 #* ))) and )())()) both result in floor -3.
 #To what floor do the instructions take Santa?
+#
+#--- Part Two ---
+#Now, given the same instructions, find the position of the first character that causes him to enter the basement (floor -1). The first character in the instructions has position 1, the second character has position 2, and so on.
+#
+#For example:
+#
+#* ) causes him to enter the basement at character position 1.
+#* ()()) causes him to enter the basement at character position 5.
+#What is the position of the character that causes Santa to first enter the basement?
+
 
 # 1) read input file from folder
 input = open("day01.txt", "r").read()
 
 # 2) count amount of ( and ) in string
+# 2a) to get the first time the difference is less than zero, calculate the difference during the iteration
 counter_opening_braces = 0
 counter_closing_braces = 0
+current_floor = 0
+position = []
 for i in input:
 	if i == "(":
 		counter_opening_braces += 1
+		current_floor += 1
 	elif i == ")":
 		counter_closing_braces += 1
+		current_floor -= 1
+	if current_floor < 0:
+		position.append(counter_opening_braces + counter_closing_braces)
 
 # 3) calculate difference between amount of ( and ) to get the floor number
 floor = counter_opening_braces - counter_closing_braces
 
 # 4) have a nice output
-print("Santa will go up", counter_opening_braces, "floors and down", counter_closing_braces, "floors in total,\nwhich results in him ending up on floor", floor,".")
+print("Santa will go up ", counter_opening_braces, " floors and down ", counter_closing_braces, " floors in total,\nwhich results in him ending up on floor ", floor,".", sep="")
 
+
+print("Santa enters the basement at position ", position[0],".", sep="")
