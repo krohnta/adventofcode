2ea9b16864d816318708d4b70dd4bbd1775de476
Teresa Krohn <tkrohn@posteo.de>
2019-12-31 13:12:50 +0100

    solve: 2016, day 3, part 2
    This is the very naïve implementation to read three lines and check the
    resulting three triangles withing each loop.
    Possible improvements:
    - read whole input at once, append the columns and check the entries in
    groups of three (ArrayList?!)
    - array of array and transpose to make columns to rows by transposing
    - ???

--- 2016/src/aoc/Day03SquaresWithThreeSides.java
+++ 2016/src/aoc/Day03SquaresWithThreeSides.java
@@ -6,6 +6,7 @@ public class Day03SquaresWithThreeSides {
 		
 	public static void main(String[] args) throws IOException {
 		partOne();
+		partTwo();
 	}
 
 	public static void partOne() throws IOException {
@@ -40,6 +41,61 @@ public class Day03SquaresWithThreeSides {
 		inputRaw.close();
 	}
 
+	public static void partTwo() throws IOException {
+		File file = new File("./puzzle/day03.txt");
+		BufferedReader inputRaw = new BufferedReader(new FileReader(file));
+
+		String[] inputOne = inputRaw.readLine().trim().split("\\s+");
+		String[] inputTwo = inputRaw.readLine().trim().split("\\s+");
+		String[] inputThree = inputRaw.readLine().trim().split("\\s+");
+		int cntTriangles = 0;
+
+		while (inputOne != null) {
+			//System.out.println(input[0] + "," + input[1] + "," + input[2]);
+			int[] intSidesOne = new int[3];
+			int[] intSidesTwo = new int[3];
+			int[] intSidesThree = new int[3];
+			// This is not a very good idea. This must be possible in a more elegant way like an array of array or reading everything at once?!
+			intSidesOne[0] = Integer.parseInt(inputOne[0]);
+			intSidesOne[1] = Integer.parseInt(inputTwo[0]);
+			intSidesOne[2] = Integer.parseInt(inputThree[0]);
+			intSidesTwo[0] = Integer.parseInt(inputOne[1]);
+			intSidesTwo[1] = Integer.parseInt(inputTwo[1]);
+			intSidesTwo[2] = Integer.parseInt(inputThree[1]);
+			intSidesThree[0] = Integer.parseInt(inputOne[2]);
+			intSidesThree[1] = Integer.parseInt(inputTwo[2]);
+			intSidesThree[2] = Integer.parseInt(inputThree[2]);
+			
+			Triangle triangleOne = new Triangle();
+			Triangle triangleTwo = new Triangle();
+			Triangle triangleThree = new Triangle();
+			triangleOne.setSides(intSidesOne);
+			triangleTwo.setSides(intSidesTwo);
+			triangleThree.setSides(intSidesThree);
+
+			if (triangleOne.isValidTriangle()) {
+				cntTriangles++;
+			}
+			if (triangleTwo.isValidTriangle()) {
+				cntTriangles++;
+			}
+			if (triangleThree.isValidTriangle()) {
+				cntTriangles++;
+			}
+			try {
+				inputOne = inputRaw.readLine().trim().split("\\s+");
+				//int tester = Integer.parseInt(input[0]);
+			} catch (NullPointerException e) {
+				System.out.println("...finished reading input file");
+				break;
+			}
+			inputTwo = inputRaw.readLine().trim().split("\\s+");
+			inputThree = inputRaw.readLine().trim().split("\\s+");
+		
+		}
+		System.out.println("Result part 2: There are " + cntTriangles + " possible triangles in the list.");
+		inputRaw.close();
+	}
 
 	static class Triangle {
 		int[] sides = null;
